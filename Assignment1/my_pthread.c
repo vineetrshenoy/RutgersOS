74dfab0956532be10ffafd2b5be2e368789db11d
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2017-02-18 15:15:08 -0500

    Context swtiching main -> printFunction -> main

--- Assignment1/my_pthread.c
+++ Assignment1/my_pthread.c
@@ -9,30 +9,29 @@
 
 
 void printFunction(){
-	printf("Entered function\n");
+	printf("Entered printFunction ONE\n");
 }
-
 
 
 
 int main(){
 	
 
-	ucontext_t ucp;	//Creates a ucontext object
-
+	ucontext_t ucp, ucp_main;	//Creates a ucontext object
+	
 	//Gets the current context and stores it in ucp. Prints error on failure
 	if (getcontext(&ucp) == -1)
 		printf("Error retrieving context\n");
 	ucp.uc_stack.ss_sp = malloc(STACK_SIZE);	//Allocate new stack space
 	ucp.uc_stack.ss_size = STACK_SIZE;			//Specify size of stack
-	ucp.uc_link = NULL;							//Sets the link to null
+	ucp.uc_link = &ucp_main;							//Sets the link to null
 
 	void (*functionPointer)();		//Create a function pointer to printFunction
 	functionPointer = &printFunction;
 
 	makecontext(&ucp, functionPointer, 0);	//assigns function printFunction in  ucp
 
-	setcontext(&ucp);			//switches to context without returning
+	swapcontext(&ucp_main, &ucp);			//switches to printFunction, executes, returnts ot main
 	
 
 	printf("Ending main\n");
