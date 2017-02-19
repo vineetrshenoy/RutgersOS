8f64a2e0bff44ff01fa960744ce9e69ada84db3f
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2017-02-18 22:00:59 -0500

    Implemented a signal handler with setitimer

--- Assignment1/my_pthread.c
+++ Assignment1/my_pthread.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ucontext.h>
 #include <signal.h>
+#include <sys/time.h>
 
 #define STACK_SIZE 1000
 
@@ -10,7 +11,7 @@ ucontext_t ucp, ucp_two, ucp_main;
 
 void printFunction(){
 	printf("Entered printFunction ONE\n");
-	swapcontext(&ucp, &ucp_two);
+	
 }
 
 
@@ -19,51 +20,27 @@ void printFunctionTwo(){
 }
 
 
+void handler(int sig){
+	printf("Entered the signal handler...now exiting\n");
+	exit(1);
+}
+
 int main(){
+	struct itimerval timer;
+	signal(SIGVTALRM,handler);
+
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = 100000;
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = 100000;
 	
-	/*
-	This function does the following
-	1. Sets all values in main (see below). Sends execution to printFunction
-	2. Prints statement, sends execution to printFunctionTwo
-	3. Prints statement, send execution back to main using uc_link (ucp_main context
-	was stored on swapcontext)
-	4. Main print "Ending main"
+	setitimer(ITIMER_VIRTUAL, &timer, NULL);
 
 
-	*/
+	while(1){
 
-		
-	
-	//Gets the current context and stores it in ucp. Prints error on failure
-	//For printFunction
-	if (getcontext(&ucp) == -1)
-		printf("Error retrieving context\n");
-	ucp.uc_stack.ss_sp = malloc(STACK_SIZE);	//Allocate new stack space
-	ucp.uc_stack.ss_size = STACK_SIZE;			//Specify size of stack
-	ucp.uc_link = &ucp_two;							//Sets the link to null
+	}
 
-
-	//for printFunctionTwo
-	if (getcontext(&ucp_two) == -1)
-		printf("Error retrieving context\n");
-	ucp_two.uc_stack.ss_sp = malloc(STACK_SIZE);	//Allocate new stack space
-	ucp_two.uc_stack.ss_size = STACK_SIZE;			//Specify size of stack
-	ucp_two.uc_link = &ucp_main;							//Sets the link to null
-
-	
-	void (*functionPointer)();		//Create a function pointer to printFunction
-	functionPointer = &printFunction;
-
-
-	void (*functionPointerTwo)();		//Create a function pointer to printFunctionTwo
-	functionPointerTwo = &printFunctionTwo;
-
-	makecontext(&ucp, functionPointer, 0);	//assigns function printFunction in  ucp
-	makecontext(&ucp_two, functionPointerTwo,0); //assigns function printFunctionTwo in  ucp_two
-
-	//Procedure: main -> printFunction -> printFunctionTwo -> main
-	swapcontext(&ucp_main, &ucp);			
-	
 
 	printf("Ending main\n");
 	return 0;
