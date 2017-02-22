644a2213d5d3f96597e697217e39ce53209e3167
r33ny <naorin.hossain@rutgers.edu>
2017-02-22 00:33:49 -0500

    add comments with pthread_mutex structs

--- Assignment1/my_pthread.c
+++ Assignment1/my_pthread.c
@@ -40,17 +40,11 @@ my_pthread_attr_t;
 //int my_pthread_create(my_pthread_t *thread, pthread_attr_t * attr, void * (*function)(void*), void* arg){
 
 	/*
-
 	First time around,this function returns two contexts.
-
-
 		1. Create a new context
 		2. Allocate stack space
 		3. Stack size
 		4. uc_link 
-
-
-
 		Have some global variable that checks if my_pthread_create has not been run before. If not,
 		1. Initialize priority queues
 		2. Create a context for the main
@@ -58,14 +52,12 @@ my_pthread_attr_t;
 		4. Only keep one version of main use version on the queue
 		
 	ucontext_t a;
-
 	getcontext(&a);
 	a.uc_link = 0;
 	a.uc_stack.ss_sp = malloc(STACK_SIZE);
 	a.uc_stack.ss_size = STACK_SIZE;
 	a.uc_stack.ss_flags = 0;
 	makecontext(&a, (void*) &function, arg);
-
 	if (!INSCHED) {
 		ready_queue.add(getcontext(&a));
 	}
@@ -81,7 +73,6 @@ my_pthread_attr_t;
 
 	/*
 		This contains schedule code
-
 	ucontext_t curr;
 	getcontext(&curr);
 	swapcontext(&curr, ready_queue.pop());
@@ -90,7 +81,6 @@ my_pthread_attr_t;
 	timer.it_interval.tv_sec = 0;
 	timer.it_value.tv_sec = 0;
 	setitimer(ITIMER_REAL, &timer, NULL);
-
 	*/
 
 //}
@@ -100,14 +90,11 @@ my_pthread_attr_t;
 
 	/*
 	ATOMIC OPERATION
-
 	1. Assign the return value of the function to value_ptr
 	2. Deallocate the stack
 	3. Remove context from the priority queue
 	4. my_pthread_yield
-
  
-
 	*/
 
 
@@ -126,18 +113,47 @@ my_pthread_attr_t;
 
 //}
 
+// struct _pthread_fastlock
+// {
+//   long int __status;
+//   int __spinlock;
+// }
+//  ;
 
+// typedef struct
+// {
+//   int __m_reserved;
+//   int __m_count;
+//   _pthread_descr __m_owner;
+//   int __m_kind;
+//   struct _pthread_fastlock __m_lock;
+// }
+// my_pthread_mutex_t;
 
+// typedef struct
+// {
+//   int __mutexkind;
+// }
+// my_pthread_mutexattr_t;
 
-//int my_thread_mutex_init(my_pthread_t * mutex, const pthread_mutexattr_t * mutexattr){
+//int my_thread_mutex_init(my_pthread_t * mutex, const my_pthread_mutexattr_t * mutexattr){
 
 //}
 
 
+// int my_pthread_mutex_lock(my_pthread_mutex_t *mutex) {
 
+	
 
+// }
 
+ // int my_pthread_mutex_unlock(my_pthread_mutex_t *mutex){
 
+ // }
+
+// int my_pthread_mutex_destroy(my_pthread_mutex_t *mutex){
+
+// }
 
 
 
@@ -173,7 +189,6 @@ int main(){
 	/*
 	struct itimerval timer;
 	signal(SIGALRM,handler);	//Creates the signal handler
-
 	//it_interval value to which reset occurs 
 	timer.it_interval.tv_sec = 0;
 	timer.it_interval.tv_usec = 100000;
@@ -183,15 +198,11 @@ int main(){
 	timer.it_value.tv_usec = 100000;
 	
 	
-
 	if (getcontext(&ucp) == -1)
 		printf("Error retrieving context\n");
 	ucp.uc_stack.ss_sp = malloc(STACK_SIZE);	//Allocate new stack space
 	ucp.uc_stack.ss_size = STACK_SIZE;			//Specify size of stack
 	ucp.uc_link = NULL;
-
-
-
 	void (*functionPointer)();
 	functionPointer = &printFunction;
 	makecontext(&ucp, functionPointer, 0);	//Creates the context
@@ -200,11 +211,9 @@ int main(){
 	
 	x = 5;
 	
-
 	
 	printf("In main ... the value of x is %d \n", x);
 	
-
 	*/
 
 	thread_node * first = (thread_node *) malloc (sizeof(thread_node));
