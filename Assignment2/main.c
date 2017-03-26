b7e7532c7fc13ac33dc3d3469b3f2e0bd99865f7
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2017-03-26 00:21:36 -0400

    Code passes msimple test case

--- Assignment2/main.c
+++ Assignment2/main.c
@@ -356,16 +356,27 @@ void callGrindF(){
 }
 
 
+void * myThread(void* p){
+        printf("Hello from thread %d\n", *(int*)p);
+    my_pthread_exit(0);
+    return 0;
+}
+
+
 int main(){
 
 	
-	srand(time(NULL));
-	printf("\n");
-	
-	
-	callGrindC();
-
-	return 0;
+	my_pthread_t* thread;
+    int id, arg1, arg2;
+    arg1 = 1;
+    thread = (my_pthread_t*) malloc(sizeof(my_pthread_t));
+    id = my_pthread_create(thread, NULL, myThread, (void*)&arg1);
+    //pthread_yield();
+    arg2 = 2;
+    my_pthread_join(*thread, NULL);
+    myThread((void*)&arg2);
+    
+    return 0;
 }
 
 
